8f214c8f2c0a4d0a0f4341673e9aeb7109fca51e
wangbo03 <taotieyishui@gmail.com>
2023-04-23 14:03:00 +0800

    init

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import json5 from 'json5';
+import fs from 'fs';
+const getRigDeps = () => {
+	let rigConfig = json5.parse(fs.readFileSync('./package.rig.json5').toString());
+	return Object.keys(rigConfig.dependencies);
+}
+const getRigGlobs = () => {
+	let rigConfig = json5.parse(fs.readFileSync('./package.rig.json5').toString());
+	return Object.keys(rigConfig.dependencies).map(dep=>`!**/${dep}/**`);
+}
+module.exports = {
+	getPkgs: getRigDeps,
+	getRigDeps,
+	getRigGlobs
+}
